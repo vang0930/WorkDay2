8f3a37e0ce938dbbca841a7caad352e00e098c3e
idsc3101 <idsc3101@gmail.com>
2014-09-17 15:28:42 -0500

    did workday 2

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -11,9 +11,16 @@ Public Class Form1
         num1 = CDbl(st1)
         num2 = CDbl(st2)
 
+        If num1 < num2 Then
+            txtFirstNum.Result.Text = "The larger number is" & num2
+        Else
+            txtResult.Text = "They are equal"
+
+        End If
         ' Your code here
 
-        txtResult.Text = "Not implemented yet"
+
+        txtResult.Text = "The larger number is" & largerNum
     End Sub
 
 End Class
